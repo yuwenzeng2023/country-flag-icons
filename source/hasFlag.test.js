a97e550ce57e0cc596ebd3e5baf865331b9e8b22
YuWen Zeng <yuwen.z@decode-group.com>
2024-02-21 13:21:30 +0800

    fix: remove package.json exports

--- source/hasFlag.test.js
+++
@@ -1,8 +0,0 @@
-import hasFlag from './hasFlag.js'
-
-describe('hasFlag', () => {
-	it('should return whether a flag icon exists', () => {
-		hasFlag('RU').should.equal(true)
-		hasFlag('ZZ').should.equal(false)
-	})
-})
